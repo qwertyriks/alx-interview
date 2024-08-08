f3cc54e541122e4dc38970d8c673a19c091156c9
qwertyriks <romeokumatse@gmail.com>
2024-08-08 18:16:56 +0000

    Task 0👌

---
+++ 0x02-minimum_operations/0-minoperations.py
@@ -0,0 +1,24 @@
+#!/usr/bin/python3
+""" A Module for 0-minoperations"""
+
+
+def minOperations(n):
+    """
+    Getting the fewest of operations needed to result in exactly n H characters
+    """
+    # Now, all outputs should be at least 2 char long: (min, Copy All => Paste)
+    if (n < 2):
+        return 0
+    ops, root = 0, 2
+    while root <= n:
+        # if n evenly divides by root,
+        if n % root == 0:
+            # the total even divisions by root = total operations
+            ops += root
+            # sets n to the remainder
+            n = n / root
+            # should reduce root to find remaining smaller values that evenly-divide n
+            root -= 1
+            # increment root until it evenly-divides n
+        root += 1
+    return ops
---
+++ 0x02-minimum_operations/README.md
@@ -0,0 +1,7 @@
+## 0x02. Minimum Operations
+Algorithm
+Python
+
+GitHub repository: alx-interview
+Directory: 0x02-minimum_operations
+File: 0-minoperations.py
