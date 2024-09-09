c1b45800562f8d72e0585a89d3e36842a11f2d9c
qwertyriks <romeokumatse@gmail.com>
2024-09-09 19:24:27 +0000

    Task 0 in progress

---
+++ 0x07-rotate_2d_matrix/0-rotate_2d_matrix.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+"""Model"""
+
+
+def rotate_2d_matrix(matrix):
+    """Documentaion for def"""
+    n = len(matrix)
+
+    for i in range(n):
+        for j in range(i, n):
+            matrix[j][i], matrix[i][j] = matrix[i][j], matrix[j][i]
+
+    for i in range(n):
+        matrix[i] = matrix[i][::-1]
