6a500a5759e9e1222f7b66941dd46d5e12a53236
qwertyriks <romeokumatse@gmail.com>
2024-09-20 02:12:29 +0000

    Task

--- 0x08-making_change/0-making_change.py
+++ 0x08-making_change/0-making_change.py
@@ -1,13 +1,14 @@
 #!/usr/bin/python3
 """Testing File"""
 
+
 def makeChange(coins, amount):
     """
     Types of coins?
     How many left. How much money left?
     How many coins in my pocket?
     """
-    
+
     if amount < 1:
         return 0
     coins.sort(reverse=True)
